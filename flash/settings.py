fe349485b17339da08e5b0ce0baf4bbf28b2c9d2
Bahram Shakiba <bahramshakiba@gmail.com>
2020-04-27 15:26:07 +0430

    tweaked for heroku

--- flash/settings.py
+++ flash/settings.py
@@ -131,3 +131,13 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
